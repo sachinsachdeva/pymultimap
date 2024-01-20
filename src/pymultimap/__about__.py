85fcab2f166d43230e509e25e7956a0b899c2ef3
sachinsachdeva <7625278+sachinsachdeva@users.noreply.github.com>
2024-01-20 18:41:35 +0100

    first working version with hatch

---
+++ src/pymultimap/__about__.py
@@ -0,0 +1,4 @@
+# SPDX-FileCopyrightText: 2024-present sachinsachdeva <7625278+sachinsachdeva@users.noreply.github.com>
+#
+# SPDX-License-Identifier: MIT
+__version__ = "0.0.1"
---
+++ src/pymultimap/__init__.py
@@ -0,0 +1,3 @@
+# SPDX-FileCopyrightText: 2024-present sachinsachdeva <7625278+sachinsachdeva@users.noreply.github.com>
+#
+# SPDX-License-Identifier: MIT
